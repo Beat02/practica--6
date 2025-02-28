ea83d11051751338ca76e6ffde3d9b1621eb9fb0
Beatriz R. Aguiar <beatriz.ruiz7@educa.madrid.org>
2025-02-28 12:38:47 +0100

    montamos consulta pdo

--- db.php
+++ db.php
@@ -1,10 +1,13 @@
 <?php
-$host='localhost';
-$dbNombre='listacompra';
-$username='root';
-$password='';
+$host = 'localhost';
+$dbNombre = 'listacompra';
+$username = 'root';
+$password = '';
 
-/* try{
+try {
     //Crear nueva PDO
-
-} */
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // Establecer el modo de error de PDO a excepciones
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+}
--- login.php
+++ login.php
@@ -1,4 +1,6 @@
 <?php
+
+require_once 'db.php';
 // Este archivo recoge los datos enviados desde el frontend, valida si coinciden con la bd y devuelve una respuesta
 
 // Recoger datos enviados por AJAX
@@ -13,8 +15,25 @@ $password = $data['password'];
 
 // Aquí iría la lógica para verificar los datos en la base de datos
 /*
-    Por ejemplo, podrías hacer una consulta SQL para verificar si el usuario
+    Hacer una consulta SQL para verificar si el usuario
     existe y si la contraseña proporcionada es correcta. */
+try {
+
+    $consulta = $pdo->prepare("SELECT * FROM usuarios WHERE email= :email");
+    $consulta->bindParam(':email', $email);
+    $consulta->execute();
+    $user = $consulta->fetch(PDO::FETCH_ASSOC); //fetch(PDO::FETCH_ASSOC): Recupera el resultado de la consulta como un array asociativo. Si el usuario existe, obtendrás sus datos.
+
+    if ($user && password_verify($password, $user['password'])) {
+        echo json_encode(['success' => true]);
+    } else {
+        echo json_encode(['success' => false]);
+    }
+} catch (PDOException $e) {
+    // Si hay algún error en la consulta, lo manejamos
+    echo json_encode(['success' => false, 'error' => 'Error en la base de datos']);
+}
+
 
 // Si el login es exitoso
 if ($email == $user && $password == $pswd) {
